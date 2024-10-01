8ad6b050b858f93aae602411694ba1dae8f0f694
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:30 +0530

    my first commit

---
+++ studentdetails.py
@@ -0,0 +1,8 @@
+print("Hello phython")
+name=input("enter the name")
+roll_number=int(input("enter the roll number:"))
+roll_number=roll_number+1
+print("Name of the student:",name)
+print ("roll_number:",roll_number)
+cgpa=float(input("enter the cgpa")
+p
