1ae646e212697502dbc06d468749df7db1a4f5c5
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-05 15:38:30 +0530

    Add files via upload

---
+++ poem lines.py
@@ -0,0 +1,7 @@
+'''
+Name ASksa Vinod
+Date 05-10-2024
+Version 1
+'''
+Str1=("Twinkle, twinkle, little star,\n\t\tHow I wonder what you are!\n\t\t\tUp above the world so high,\n\t\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\t\tHow I wonder what you are")
+print(Str1)
