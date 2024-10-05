2e80a83413733db3612bf8cfae58acf2e717785b
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-05 15:45:56 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,13 @@
+'''
+Name Aksa Vinod
+Date 05-10-2024
+version 1
+'''
+numbera=10
+numberb = 5
+print (numbera+numberb)
+print(numbera/numberb)
+print(numbera>numberb)
+print(numbera==numberb)
+print(numbera>numberb and numberb<numbera)
+print(numbera>numberb or numberb>numbera)
