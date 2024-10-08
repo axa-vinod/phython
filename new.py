b5672bb0911b35b4995ca8f7a1a8878a5e27b5c8
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-08 15:15:36 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,19 @@
+"""
+Name Aksa Vinod
+Date 08-10-2024
+version 1.1
+"""
+number1=int(input("Enter your first number"))
+number2=int(input("Enter your second number"))
+number3=int(input("Enter your third number"))
+print("num1=",number1,"num2=",number2)
+addition=(number1+number2)
+print("The sum of num1 and num2:",addition)
+subtraction=(number2-number1)
+print("The difference when num2 is subtracted from num1:",subtraction)
+multiply=(number1*number2)
+print("The product of num1 and num2:",multiply)
+modulus=(number1%number2)
+print("The remainder when num1 is divided by number2 is:",modulus)
+combination=((number1+number2)*number3/2)
+print("The result of (num1+num2)*num3/2is:", combination)
