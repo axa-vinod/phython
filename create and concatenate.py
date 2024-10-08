3a4f0ebbf800aa21e7dcb9bde566a45ccd665bb8
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-08 15:15:03 +0530

    Add files via upload

---
+++ create and concatenate.py
@@ -0,0 +1,12 @@
+"""
+Name Aksa Vinod
+Date 08-10-2024
+verdion1.1
+"""
+first_name=input("Enter your first name")
+last_name=input("Enter your last name")
+full_name=(first_name+" "+last_name)
+print(full_name)
+length=len(first_name)
+extracted_first_name=full_name[:length]
+print(extracted_first_name)
