ea6212e95e1e8116dd5e5a3707d5252e2ee8f4c7
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-15 14:03:26 +0530

    Add files via upload

---
+++ If and else.py
@@ -0,0 +1,12 @@
+"""
+Name AKsa Vinod
+Date 15-10-2024
+version1.1
+python programme to check whether the given number
+is Positive or Negative
+"""
+number=int(input("Enter a number:"))
+if number>0:
+     print("The given number:",number," is positive")
+else:
+      print("The given number:",number," is negative")
