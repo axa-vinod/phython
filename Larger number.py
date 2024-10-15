f1c22efc8471aaf197b9bf3fcd6907d058cdb7ee
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-15 15:22:09 +0530

    Add files via upload

---
+++ Larger number.py
@@ -0,0 +1,11 @@
+"""
+Name Aksa Vinod
+Date 15-10-2024
+Version 1.1
+"""
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+if num1>num2:
+    print("number1 is larger than number2")
+else:
+    print("number2 is larger than number1")
