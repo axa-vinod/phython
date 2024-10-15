d1ba9e5157f5aee645728c33915ce3e01c38fc07
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-15 15:58:38 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,14 @@
+"""
+Name Aksa Vinod
+Date 15-10-2024
+Version1.1
+python programme to generate n odd number
+"""
+limit=int(input("Enter the limit:"))
+odd_number=1
+count=0
+while count<limit:
+    print(odd_number,"\t",end="")
+    count+=1
+    odd_number+=2
+
