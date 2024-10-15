d8bb694abf6c5bc0ac0007c7d6762bd5b49af075
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-15 15:11:59 +0530

    Add files via upload

---
+++ Simple Interest.py
@@ -0,0 +1,13 @@
+"""
+Name Aksa Vinod
+Date 15-10-2024
+Version1.1
+"""
+amount=int(input("Enter your principal amount"))
+print("Your principal amount is",amount)
+Year=int(input("Enter your number of years"))
+print("Your number of years is",Year)
+interest=int(input("enter your rate of interest"))
+print("Your rate of interest is",interest,"%")
+simple_interest=amount*Year*interest
+print("Your simple interest is",simple_interest)
