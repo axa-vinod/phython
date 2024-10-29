8b7fd674b72d42f36e8c23c7fe44320b86ecc69a
axa-vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-10-29 15:44:45 +0530

    Add files via upload

---
+++ multipication table.py
@@ -0,0 +1,10 @@
+"""
+Name Aksa VInod
+Date 29-10-2024
+Version1.1
+"""
+num=int(input("Enter your number"))
+for i in range (1,13):
+    multiplication=num*i
+    print(f"{num}\t*\t{i}\t={multiplication}\t")
+
