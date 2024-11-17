e436ea432d5e7cb4b386aa8948ae3217af3fb7ec
Aksa Vinod <aksavinod2028@cs.sjcetpalai.ac.in>
2024-11-17 21:50:56 +0530

    Add files via upload

---
+++ loop in multipiction.py
@@ -0,0 +1,11 @@
+"""
+Aksa Vinod
+Date 12-11-2024
+Version1.1
+"""
+num=[1500,2700]
+for num in range(1500,2701,1):
+    if num %7==0:
+        print(f"The number divisible by 7 is:{num}")
+        if  num %5==0:
+         print(f"The multiples of 5 is:{num}")
